eca6a5b6ca40c99bc956f8f50750e362343fb209
Olha <olgaklim510@gmail.com>
2022-10-14 14:56:23 +0300

    Created App.

---
+++ src/main/java/com/company/unitTests/App.java
@@ -0,0 +1,23 @@
+package com.company.unitTests;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Please enter number");
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            int n = scanner.nextInt();
+            int result = new SumCalculator().sum(n);
+            System.out.println("Sum numbers from 1 to " + n + " equals: " + result + "\n");
+            System.out.println("Do you want to continue? (yes or no)");
+            if (scanner.next().equalsIgnoreCase("yes")) {
+                System.out.println("Please enter number");
+                continue;
+            }
+            if (scanner.next().equalsIgnoreCase("no")) {
+                break;
+            }
+        }
+    }
+}
